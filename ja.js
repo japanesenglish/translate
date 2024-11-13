35e2712f1c67c7166f7f86c9f7c17112bcea26f8
japanesenglish <hashiyuu0622@gmail.com>
2024-11-13 23:28:54 +0900

    Update ja.js

--- ja.js
+++ ja.js
@@ -12,3 +12,31 @@ a.forEach(function(a99){
         };
     });
 });
+
+let b = document.querySelectorAll('.start');
+let by = []
+b.forEach(function(b99){
+    let bp = b99.getBoundingClientRect().top
+    by.push(Math.floor(bp));
+})
+document.addEventListener('scroll',function(){
+let count = 0
+let before = -1
+by.forEach(function(by99){
+            let y = window.scrollY + window.innerHeight;
+            console.log(y,by99,count);
+            let head = document.getElementById(count);
+            let beforehead = document.getElementById(before);
+            if(y > by99 && head.classList.contains('open') == false){
+                head.classList.toggle('open')
+                head.style.width = head.scrollWidth + 10 + 'px';
+                head.style.backgroundColor = '#00000044'
+                beforehead.style.width = '';
+                beforehead.style.right = 0 + 'px';
+                console.log(count)
+            }
+            count = count + 1; 
+            before = before + 1;
+    })
+
+})
